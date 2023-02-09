f95e160cd8977b0d064bf01c10466e37c0cb15dd
dev-vince <dev-vince@users.noreply.github.com>
2023-02-09 13:40:52 +0000

    deploy: 1181eff2facbc7872d4c2e39cda37fe2f7319752

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"dev.vince.colorify"},{"l":"dev.vince.colorify.ansi"},{"l":"dev.vince.colorify.format"},{"l":"dev.vince.colorify.util"}];updateSearchResults();
